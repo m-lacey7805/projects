d5abf0cfb5f0e4886aae221921d7d3a397569516
m-lacey7805 <mlacey7805@sohamcollege.org.uk>
2014-12-18 14:38:01 +0000

    18/12/14

--- Guess Who project/Picture taking function.py
+++ Guess Who project/Picture taking function.py
@@ -1,6 +1,6 @@
-import picamera,time
+import picamera,time,json
 
-def getCharProfile():
+def getCharProfile(name):
     try:
         with picamera.PiCamera() as camera:
             check = False
@@ -12,7 +12,6 @@ def getCharProfile():
                 camera.stop_preview()
                 if input("Do u like ur Picture?[y/n]") == "y":
                         check = True
-
     except ("picamera.exc.PiCameraMMALError:","picamera.exc.PiCameraError:"):
         print("your Camera is not detected, please insert PiCamera and resart")
         filename = ""
@@ -28,11 +27,10 @@ def getCharProfile():
     while not (hair in ["blue","brown","green"]):
         eye = input("what is your eye colour?")    
 
-    glasses = input("Do you have glasses?")
+    glasses = input("Do you have glasses?[yes/no]")
 
-    facialhair = input("Do you have FacialHair?")
+    facialhair = input("Do you have FacialHair?[yes/no]")
 
-import json
 
 def load():
     try:
@@ -43,9 +41,11 @@ def load():
         people= []
     return people
 
-#def store():
-    #people = getCharProfile():
-    
+def store():
+    people = getCharProfile()
+    with open("people",mode='w') as file:
+        json.dump(people,file)
+    return people
 
 
 
@@ -53,5 +53,6 @@ def load():
 
 
 
-#people = load()
-
+people = load()
+print(people)
+people =store(people)
